a70748dc1e67ee31a25bf2b56493412f22110232
pure-soul-kk <krishnakripa34567@gmail.com>
2023-09-29 19:07:03 +0530

    sweet: switch back to sleepy kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,6 +14,6 @@ git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_sm615
 git clone --depth=1 -b 13-new https://gitlab.com/pure-soul-kk/vendor_xiaomi_sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 git clone --depth=1 https://github.com/ProjectElixir-Devices/hardware_xiaomi -b sweet hardware/xiaomi
 git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/r498229b
-git clone --depth=1 https://github.com/pure-soul-kk/zelo_kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone --depth=1 https://github.com/pure-soul-kk/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
 git clone https://github.com/pure-soul-kk/sony_vendor_dolby vendor/dolby
 git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
