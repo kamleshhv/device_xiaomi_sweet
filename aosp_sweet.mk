790c5855085939a665c36bc2ba45693fbb2bfbf3
K R I S H N A <krishnakripa34567@gmail.com>
2023-11-03 19:49:13 +0000

    sweet: init Project Elixir

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2021 The LineageOS Project
+# Copyright (C) The Project Elixir
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,10 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Project Elixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# Elixir Officials
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+ELIXIR_BUILD_TYPE := OFFICIAL
+TARGET_CALL_RECORDING_SUPPORTED := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
