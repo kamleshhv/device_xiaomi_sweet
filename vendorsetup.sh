2297314d159fc64d3482f18365ba06e7a7fc9bfc
kamleshhv <kamleshvansajalia@gmail.com>
2023-12-16 13:48:05 +0530

    Sweet: track device source

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+git clone -b fourteen https://github.com/kamleshhv/device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+
+git clone -b thirteen https://github.com/kamleshhv/proprietary_vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+
+git clone -b thirteen https://github.com/kamleshhv/proprietary_vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+
+git clone --depth=1 -b fourteen https://github.com/omnitrix21/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
