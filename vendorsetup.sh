cb08eea4b64ec2ccd4b61099ecb6aac22f9ed9a9
kamleshhv <kamleshvansajalia@gmail.com>
2023-12-30 10:28:17 +0530

    Sweet: switch to zelo kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,6 @@ git clone -b thirteen https://github.com/kamleshhv/proprietary_vendor_xiaomi_swe
 
 git clone -b thirteen https://github.com/kamleshhv/proprietary_vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
 
-git clone --depth=1 -b zelo-13 https://github.com/zelodev/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone --depth=1 https://github.com/zelodev/kernel_xiaomi_sm6150.git -b zelo-13 kernel/xiaomi/sm6150
+
+git clone -b UNO https://github.com/pure-soul-kk/vendor_xiaomi_sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
