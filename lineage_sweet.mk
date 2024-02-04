365e48551b6a8808d585fa74256b9087b0cdfd31
Kamlesh <81345800+kamleshhv@users.noreply.github.com>
2024-02-04 13:46:04 +0530

    sweet: Official Project Matrixx Bringup

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -11,14 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Project Matrixx stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_sweet
+PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#Project Matrixx stuff
+MATRIXX_BUILD_TYPE := official
+MATRIXX_CHIPSET := Qualcomm-sm7150
+MATRIXX_BATTERY := 5020mah
+MATRIXX_DISPLAY := 1080x2400
+MATRIXX_MAINTAINER := kamlesh
+WITH_GMS := true
+
---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# Common Device Tree
+git clone -b matrixx https://github.com/kamleshhv/device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+
+# Vendor Tree
+git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+
+# Common Vendor Tree
+git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+
+# Miui Camera
+git clone -b leica14 https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
+
+# Kernel Tree
+git clone --depth=1 -b fourteen-old https://github.com/omnitrix21/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
