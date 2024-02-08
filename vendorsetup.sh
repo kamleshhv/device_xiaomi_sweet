202a0625c14d7de2bd5b75b21da5100351110cfa
kamleshhv <kamleshvansajalia@gmail.com>
2024-02-08 15:57:51 +0530

    sweet: switch to proton clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,10 +8,11 @@ git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sweet.git vendo
 git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
 
 # Miui Camera
-git clone -b leica14 https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
+git clone -b leica-holybear https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 
 # Kernel Tree
 git clone --depth=1 -b inline-rom https://github.com/fiqri19102002/android_kernel_xiaomi_sweet.git kernel/xiaomi/sm6150
 
 # Clang
-git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang.git prebuilts/clang/host/linux-x86/cosmic-clang
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+
