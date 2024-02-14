f1a8c915d30d617665be852858800afb7851a9e6
kamleshhv <kamleshvansajalia@gmail.com>
2024-02-14 10:53:44 +0530

    sweet: switch to leica holybear

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sweet.git vendo
 git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
 
 # Miui Camera
-git clone -b leica14 https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
+git clone -b leica-holybear https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 
 # Kernel Tree
 git clone --depth=1 -b inline-rom https://github.com/fiqri19102002/android_kernel_xiaomi_sweet.git kernel/xiaomi/sm6150
