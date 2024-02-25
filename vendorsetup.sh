e1285e0425d3c2c9660c01bc09dbc5d13d1a6f7c
Kamlesh <kamleshvansajalia@gmail.com>
2024-02-25 10:17:12 +0530

    sweet: switch to perf kernel and playground clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,8 +11,8 @@ git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sm6150-common.g
 git clone -b leica-holybear https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 
 # Kernel Tree
-git clone --depth=1 -b zelo-13 https://github.com/kamleshhv/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone --depth=1 -b perf https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
 
 # Clang
-git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+git clone --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playgroundtc
 
