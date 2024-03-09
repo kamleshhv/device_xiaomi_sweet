bba3f0f6a2c5a7d12846829af96950eb5afe295d
Kamlesh <kamleshvansajalia@gmail.com>
2024-03-09 19:23:53 +0530

    sweet: switch to strixx kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,5 +11,5 @@ git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sm6150-common.g
 git clone -b leica-holybear https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 
 # Kernel Tree
-git clone --depth=1 -b perf https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone --depth=1 https://github.com/kamleshhv/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
 
