60971e77ea8d8e8514495afb683870053f6d8158
Kamlesh <kamleshvansajalia@gmail.com>
2024-03-09 19:23:53 +0530

    sweet: switch to strixx kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
+
 git clone https://github.com/kamleshhv/device_xiaomi_sm6150-common.git -b tiramisu device/xiaomi/sm6150-common
 git clone https://github.com/kamleshhv/vendor_xiaomi_sweet.git -b thirteen vendor/xiaomi/sweet
 git clone https://github.com/kamleshhv/vendor_xiaomi_sm6150-common.git -b thirteen vendor/xiaomi/sm6150-common
-git clone --depth=1 -b perf https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
 git clone --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playgroundtc
+git clone --depth=1 https://github.com/kamleshhv/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
