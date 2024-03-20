5bb45a7616e39e376510880a0ae72ab80072b2af
kamleshhv <kamleshvansajalia@gmail.com>
2024-03-20 11:42:45 +0000

    sweet: track repo with vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+# Common Device Tree
+git clone -b zeph https://github.com/kamleshhv/device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+
+# Vendor Tree
+git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+
+# Common Vendor Tree
+git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+
+# Miui Camera
+git clone -b leica14 https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
+
+# Kernel Tree
+git clone --depth=1 -b fourteen-old https://github.com/omnitrix21/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+
+#clang
+git clone --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/playgroundtc-clang
+
+#hardware/xiaomi
+git clone https://github.com/kamleshhv/android_hardware_xiaomi.git hardware/xiaomi
