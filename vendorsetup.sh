84fff9390181fea2d4e0249290503e8b1f07f7f9
kamleshhv <kamleshvansajalia@gmail.com>
2024-03-23 05:03:08 +0000

    sweet: vendorsetup.sh: switch to prebuilt kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,18 @@
 # Common Device Tree
-rm -rf device/xiaomi/sm6150-common
 git clone -b UNO https://github.com/ProjectElixir-Devices/device_xiaomi_sm6150-common_sweet device/xiaomi/sm6150-common
 
 # Vendor Tree
-rm -rf vendor/xiaomi
-git clone --depth=1 -b UNO https://github.com/ProjectElixir-Devices/vendor_xiaomi_sweet vendor/xiaomi/sweet
-git clone --depth=1 -b UNO https://github.com/ProjectElixir-Devices/vendor_xiaomi_sm6150-common-sweet vendor/xiaomi/sm6150-common
+git clone -b UNO https://github.com/ProjectElixir-Devices/vendor_xiaomi_sweet vendor/xiaomi/sweet
+git clone -b UNO https://github.com/ProjectElixir-Devices/vendor_xiaomi_sm6150-common-sweet vendor/xiaomi/sm6150-common
 
 # Miui Camera
-git clone --depth=1 https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
+git clone https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 
-# Kernel Tree
-rm -rf kernel/xiaomi
-git clone --depth=1 https://github.com/pure-soul-kk/kernel_xiaomi_sweet kernel/xiaomi/sm6150
+#hardware/xiaomi
+git clone https://github.com/kamleshhv/android_hardware_xiaomi.git hardware/xiaomi
 
+#delete hardware/xiaomi source one
+rm -rf vendor/hardware/xiaomi
+
+#prebuilt-kernel
+git clone  https://github.com/kamleshhv/android_device_xiaomi_sweet-kernel.git device/xiaomi/sweet-kernel
