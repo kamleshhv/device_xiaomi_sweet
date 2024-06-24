b4e2f4e2fd03f4df76a0b882ac7d77405ffb7d80
kamleshhv <kamleshvansajalia@gmail.com>
2024-06-24 09:11:48 +0530

    sweet: switch to strixx kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,12 +9,12 @@ git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sm6150-common v
 git clone -b leica-holybear https://gitlab.com/kamleshhv/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 
 # Xiaomi Hardware
-rm -rf hardware/xiaomi
-git clone https://github.com/ProjectElixir-Devices/hardware_xiaomi hardware/xiaomi
+#rm -rf hardware/xiaomi
+#git clone https://github.com/ProjectElixir-Devices/hardware_xiaomi hardware/xiaomi
 
 # Kernel Tree
-git clone --depth=1 -b sleepy-inline https://github.com/kamleshhv/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
+git clone --depth=1 -b fourteen https://github.com/unmoved21/kernel_xiaomi_sweet.git kernel/xiaomi/sm6150
 
 #clang
-rm -rf prebuilts/clang/host/linux-x86/clang-r498229b
-git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git prebuilts/clang/host/linux-x86/clang-r498229b
+#rm -rf prebuilts/clang/host/linux-x86/clang-r498229b
+#git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git prebuilts/clang/host/linux-x86/clang-r498229b
