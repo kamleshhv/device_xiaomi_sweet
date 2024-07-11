38ddbb20f398df81895a8a44515920b9d26bf7d6
kamleshhv <kamleshvansajalia@gmail.com>
2024-07-11 05:04:49 +0000

    sweet: Build missing libraries for 14 QPR3

--- device.mk
+++ device.mk
@@ -365,7 +365,8 @@ PRODUCT_COPY_FILES += \
 
 # Network
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.1.vendor
+    android.system.net.netd@1.1.vendor \
+    libnetutils.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
@@ -442,7 +443,8 @@ PRODUCT_PACKAGES += \
     libjson \
     libril \
     librilutils \
-    librmnetctl
+    librmnetctl \
+    libsqlite.vendor
 
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
