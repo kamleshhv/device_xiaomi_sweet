e366d0f912d680859dd7c3ebc9a001dfb09f8ccb
kamleshhv <kamleshvansajalia@gmail.com>
2024-07-25 11:06:46 +0530

    sweet: track repo with vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,17 @@
+#device sm6150-common
+git clone https://github.com/kamleshhv/device_xiaomi_sm6150-common.git -b mat device/xiaomi/sm6150-common
+
+#vendor
+git clone https://gitlab.com/crdroidandroid/proprietary_vendor_xiaomi_sweet.git -b 14.0 vendor/xiaomi/sweet
+
+#vendor sm6150-common
+git clone https://github.com/crdroidandroid/proprietary_vendor_xiaomi_sm6150-common.git -b 14.0 vendor/xiaomi/sm6150-common
+
+#kernel
+git clone --depth=1 https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git -b sleepy-inline kernel/xiaomi/sm6150
+
+#miui camera
+git clone -b leica-holybear https://gitlab.com/kamleshhv/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
+
+#hardware xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
