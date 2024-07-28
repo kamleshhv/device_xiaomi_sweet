d2f761ff836ecb5eab24055e7dfeff2366ce5459
kamleshhv <kamleshvansajalia@gmail.com>
2024-07-28 04:09:19 +0000

    sweet: welcome AuroraDroid

--- aurora_sweet.mk
+++ aurora_sweet.mk
@@ -13,11 +13,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common Project Elixir stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, vendor/aurora/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := blaze_sweet
+PRODUCT_NAME := aurora_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
@@ -30,11 +30,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sweet_global-user 13 TKQ1.221013.002 V14.0.9.0.TKFMIXM release-keys"
 
 # Blaze Official Stuff
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := kamlesh
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_BLUR := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USE_PIXEL_CHARGER := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-WITH_GAPPS := true
+AURORA_BUILD_TYPE := OFFICIAL
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,12 @@
 # Common Device Tree
-git clone -b blaze https://github.com/kamleshhv/device_xiaomi_sm6150-common device/xiaomi/sm6150-common
+git clone -b aur https://github.com/kamleshhv/device_xiaomi_sm6150-common device/xiaomi/sm6150-common
 
 # Vendor Tree
 git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sweet vendor/xiaomi/sweet
 git clone -b fourteen https://github.com/kamleshhv/vendor_xiaomi_sm6150-common vendor/xiaomi/sm6150-common
 
 # Miui Camera
-git clone -b leica-holybear https://gitlab.com/kamleshhv/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
-
-# Xiaomi Hardware
-rm -rf hardware/xiaomi
-git clone https://github.com/ProjectElixir-Devices/hardware_xiaomi hardware/xiaomi
+#git clone -b leica-holybear https://gitlab.com/kamleshhv/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 
 # Kernel Tree
 git clone --depth=1 -b fourteen https://github.com/unmoved21/kernel_xiaomi_sweet.git kernel/xiaomi/sm6150
-
-#hardware/qcom-caf/common
-rm -rf hardware/qcom-caf/common
-git clone -b lineage-21.0 https://github.com/LineageOS/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
