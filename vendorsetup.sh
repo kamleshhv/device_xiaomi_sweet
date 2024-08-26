b0619408cdcb9f4c9cc1834248379071590c5f2e
kamleshhv <kamleshvansajalia@gmail.com>
2024-08-26 15:08:26 +0000

    sweet: welcome project everest

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,10 +8,10 @@ git clone https://github.com/kamleshhv/vendor_xiaomi_sweet.git -b mat vendor/xia
 git clone https://github.com/crdroidandroid/proprietary_vendor_xiaomi_sm6150-common.git -b 14.0 vendor/xiaomi/sm6150-common
 
 #kernel
-git clone --depth=1 https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git -b sleepy-inline kernel/xiaomi/sm6150
+git clone --depth=1 https://github.com/kamleshhv/kernel_xiaomi_sweet.git -b 14 kernel/xiaomi/sm6150
 
 #miui camera
-git clone -b leica-holybear https://gitlab.com/kamleshhv/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
+git clone -b fourteen https://github.com/pixelos-devices/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 
 #hardware xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
