085743a7303c30ffbb8d01b8333cf91f32a12d0c
Kamlesh <81345800+kamleshhv@users.noreply.github.com>
2024-10-20 17:10:07 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 git clone -b fourteen https://github.com/PixelOS-Devices/kernel_xiaomi_sm6150.git --depth=1 kernel/xiaomi/sweet
 git clone -b U https://github.com/omnitrix21/vendor_xiaomi_sweet vendor/xiaomi/sweet
-git clone -b U https://github.com/mrfox2003/sweet-miuicamera vendor/xiaomi/sweet-miuicamera
+git clone -b U https://github.com/unmoved21/sweet-miuicamera vendor/xiaomi/sweet-miuicamera
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX  packages/apps/ViPER4AndroidFX
 rm -rf hardware/xiaomi
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX  packages/apps/ViPER4AndroidFX
+git clone https://github.com/omnitrix21/hardware_xiaomi hardware/xiaomi
